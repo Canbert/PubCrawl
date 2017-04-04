26f284faeeeff9fe5d6a090aca238f4743dffebb
Canbert <thompus987@gmail.com>
2017-04-04 19:08:53 +0100

    Google map displaying

--- app/src/main/java/com/rgu/scott1508551/pubcrawl/GenerateActivity.java
+++ app/src/main/java/com/rgu/scott1508551/pubcrawl/GenerateActivity.java
@@ -1,13 +1,29 @@
 package com.rgu.scott1508551.pubcrawl;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-public class GenerateActivity extends AppCompatActivity {
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class GenerateActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate);
+        MapFragment mapFrag = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
+        mapFrag.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
     }
 }
