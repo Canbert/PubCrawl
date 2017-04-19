ed2defb3418c2882702d5649cb902a850ebfc24e
Canbert <thompus987@gmail.com>
2017-04-19 21:24:53 +0100

    Added map fragment stuff

--- app/src/main/java/com/rgu/scott1508551/pubcrawl/EditCrawlActivity.java
+++ app/src/main/java/com/rgu/scott1508551/pubcrawl/EditCrawlActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 
-public class EditCrawlActivity extends AppCompatActivity {
+public class EditCrawlActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     Bundle data;
     LatLng latLng;
@@ -16,6 +19,9 @@ public class EditCrawlActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_crawl);
+
+        MapFragment mapFrag = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
+        mapFrag.getMapAsync(this);
 
         //set the bundle to the bundle sent from generate activity
         data = getIntent().getExtras();
@@ -27,6 +33,11 @@ public class EditCrawlActivity extends AppCompatActivity {
 
         Log.d("Edit Crawl latLng", String.valueOf(latLng));
         Log.d("Edit Crawl numPubs", String.valueOf(numPubs));
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
 
     }
 }
