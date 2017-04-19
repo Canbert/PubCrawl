949b3120ecc6bbe76adeec76675ea9aff0512ef0
Canbert <thompus987@gmail.com>
2017-04-19 20:10:36 +0100

    Added edit crawl bundle parsing

--- app/src/main/java/com/rgu/scott1508551/pubcrawl/EditCrawlActivity.java
+++ app/src/main/java/com/rgu/scott1508551/pubcrawl/EditCrawlActivity.java
@@ -2,12 +2,31 @@ package com.rgu.scott1508551.pubcrawl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
 
 public class EditCrawlActivity extends AppCompatActivity {
+
+    Bundle data;
+    LatLng latLng;
+    int numPubs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_crawl);
+
+        //set the bundle to the bundle sent from generate activity
+        data = getIntent().getExtras();
+        Log.d("Edit Crawl Bundle", data.toString());
+
+        //set the values of latLng and numPubs from the bundle
+        latLng = new LatLng(data.getDouble("lat"), data.getDouble("lng"));
+        numPubs = data.getInt("numPubs");
+
+        Log.d("Edit Crawl latLng", String.valueOf(latLng));
+        Log.d("Edit Crawl numPubs", String.valueOf(numPubs));
+
     }
 }
