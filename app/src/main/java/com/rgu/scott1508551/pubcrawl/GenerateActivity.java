cc7be95dc7f1fef5ac2c5ff460df59e873294f4d
Canbert <thompus987@gmail.com>
2017-04-19 17:32:48 +0100

    Added seekbar listener

--- app/src/main/java/com/rgu/scott1508551/pubcrawl/GenerateActivity.java
+++ app/src/main/java/com/rgu/scott1508551/pubcrawl/GenerateActivity.java
@@ -2,16 +2,20 @@ package com.rgu.scott1508551.pubcrawl;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 
-public class GenerateActivity extends FragmentActivity implements OnMapReadyCallback {
+public class GenerateActivity extends FragmentActivity implements OnMapReadyCallback, SeekBar.OnSeekBarChangeListener {
 
     private GoogleMap map;
+    private Button btnGenerate;
+    private SeekBar seekBarPubs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +24,28 @@ public class GenerateActivity extends FragmentActivity implements OnMapReadyCall
         MapFragment mapFrag = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
         mapFrag.getMapAsync(this);
 
+        //assign the button, and seekbar
+        btnGenerate = (Button)this.findViewById(R.id.btnCreate);
+        seekBarPubs = (SeekBar)this.findViewById(R.id.seekBarPubs);
+
+        //set seekbar on change listener
+        seekBarPubs.setOnSeekBarChangeListener(this);
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
     }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        Log.d("Num Pubs",String.valueOf(seekBarPubs.getProgress() + 1));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {}
 }
