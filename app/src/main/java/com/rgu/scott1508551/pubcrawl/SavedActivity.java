4488ceea38c7620e06446d6eb61b6a430b4ecbb5
Canbert <thompus987@gmail.com>
2017-04-23 20:07:30 +0100

    Saved querying

--- app/src/main/java/com/rgu/scott1508551/pubcrawl/SavedActivity.java
+++ app/src/main/java/com/rgu/scott1508551/pubcrawl/SavedActivity.java
@@ -2,12 +2,46 @@ package com.rgu.scott1508551.pubcrawl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
-public class SavedActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class SavedActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
+
+    private ListView savedList;
+    private ArrayAdapter adapter;
+
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved);
+
+        savedList = (ListView)this.findViewById(R.id.listViewSaved);
+        searchView = (SearchView)this.findViewById(R.id.searchView);
+
+        ArrayList testArray = new ArrayList();
+        testArray.add("Hwell");
+        testArray.add("sttff");
+
+        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,testArray);
+        savedList.setAdapter(adapter);
+
+        searchView.setOnQueryTextListener(this);
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        SavedActivity.this.adapter.getFilter().filter(newText);
+        return false;
     }
 }
