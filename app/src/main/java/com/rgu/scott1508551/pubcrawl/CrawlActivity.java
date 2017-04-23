f1c025a32ec852c51a739b238357ec8559831895
Canbert <thompus987@gmail.com>
2017-04-23 16:54:15 +0100

    Crawl oncreate stuff

--- app/src/main/java/com/rgu/scott1508551/pubcrawl/CrawlActivity.java
+++ app/src/main/java/com/rgu/scott1508551/pubcrawl/CrawlActivity.java
@@ -2,12 +2,55 @@ package com.rgu.scott1508551.pubcrawl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class CrawlActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class CrawlActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener{
+
+    private GoogleMap map;
+    private ImageView barImage;
+    private TextView barName;
+    private TextView barLocation;
+    private Button btnNext;
+
+    private Bundle data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crawl);
+
+        //set the map
+        MapFragment mapFrag = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
+        mapFrag.getMapAsync(this);
+
+        //set all other elements
+        barImage = (ImageView)this.findViewById(R.id.imageViewBarPicture);
+        barName = (TextView)this.findViewById(R.id.textViewBarName);
+        barLocation = (TextView)this.findViewById(R.id.textViewBarLocation);
+        btnNext = (Button)this.findViewById(R.id.btnNext);
+
+        //button listener
+        btnNext.setOnClickListener(this);
+
+        //set the bundle to the bundle sent from generate activity
+        data = getIntent().getExtras();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
